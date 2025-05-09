69a4eaea26a91dcf76ab380bb6b8f73b3aaa6d53
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-05-09 14:01:10 +0330

    Add sample api

---
+++ src/MyApi/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var swaggerConfig = configuration.GetSection("Swagger");
+    var authUrl = swaggerConfig["AuthorizationUrl"];
+    var tokenUrl = swaggerConfig["TokenUrl"];
+    var clientId = swaggerConfig["ClientId"];
+    var scopes = swaggerConfig.GetSection("Scopes").Get<Dictionary<string, string>>();
+
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri(authUrl),
+                TokenUrl = new Uri(tokenUrl),
+                Scopes = scopes
+            }
+        }
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "oauth2",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            ["myapi.read"]
+        }
+    });
+});
+builder.Services
+    .AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = configuration["Authentication:Authority"];
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MyApiPolicy", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "myapi.read");
+    });
+});
+
+var app = builder.Build();
+app.UseAuthentication();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.OAuthClientId(configuration["Swagger:ClientId"]);
+        options.OAuthUsePkce();
+        options.OAuthScopeSeparator(" ");
+        options.OAuthAppName("My API - Swagger");
+    });
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
