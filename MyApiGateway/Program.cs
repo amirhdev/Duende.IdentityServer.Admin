a03019f151806a4ba3c7d79fd20de8511207348e
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-05-28 23:20:27 +0330

    Add apigateway project

---
+++ MyApiGateway/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// خواندن تنظیمات Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+/*builder.Services.AddSwaggerGen();*/
+
+// احراز هویت JWT مطابق با MyApi
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:44310"; // مقدار دقیق را از MyApi بردارید
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseAuthentication();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerForOcelotUI(opt =>
+    {
+        opt.PathToSwaggerGenerator = "/swagger/docs";
+    });
+}
+app.UseAuthorization();
+await app.UseOcelot();
+
+app.Run();
